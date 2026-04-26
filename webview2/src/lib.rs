93bde1da5918313d88cc7ba937e73ee97a2d1a0c
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-04-26 23:19:16 +0800

    Update bindgen & remove C fns

--- webview2/src/lib.rs
+++ webview2/src/lib.rs
@@ -1,13 +1,2 @@
 mod bindings;
 pub use bindings::*;
-
-macro_rules! __link {
-    ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-        extern $abi {
-            $(#[link_name=$link_name])?
-            pub fn $($function)*;
-        }
-    )
-}
-
-pub(crate) use __link as link;
